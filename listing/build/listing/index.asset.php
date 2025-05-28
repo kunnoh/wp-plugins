0822183611fbf50b9bb7e3aaf1ca5fffd7b1d72a
kunnoh <alvindereba@gmail.com>
2025-05-28 10:46:55 +0300

    added new block

---
+++ listing/build/listing/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '6b68981f0ea5d8c07478');
---
+++ listing/build/listing/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e8da124698227f589fba');
