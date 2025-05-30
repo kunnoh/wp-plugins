509795a57942a7a714988df23eed92604fd0bbc2
kunnoh <alvindereba@gmail.com>
2025-05-31 00:14:04 +0300

    npmm start

--- listing/build/listing/index.asset.php
+++ listing/build/listing/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '2c98e9c190662f8adf22');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8607efa8e32c2b9fecef');
