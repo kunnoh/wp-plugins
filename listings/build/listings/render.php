335a643aa6eeadf30c849ddc8185bd31a45e8b87
kunnoh <alvindereba@gmail.com>
2025-06-19 16:13:25 +0300

    render showing posts

--- listings/build/listings/render.php
+++ listings/build/listings/render.php
@@ -2,11 +2,39 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+
+$listing_to_show = isset($attributes['listingsToShow']) ? intval($attributes['listingsToShow']) : 10;
+$order = isset($attributes['order']) ? $attributes['order'] : 'desc';
+$order_by = isset($attributes['orderBy']) ? $attributes['orderBy'] : 'date';
+
+$args = array(
+    'post_type' => 'listing',
+    'posts_per_page' => $listing_to_show,
+    'post_status' => 'publish',
+    'order' => $order,
+    'orderby' => $order_by,
+    'ignore_sticky_posts' => true,
+    'no_found_rows' => true,
+);
+
+// Get posts
+$listings = new WP_Query($args);
 ?>
+
 <div <?php echo get_block_wrapper_attributes(); ?>>
-    <p>	
-        <?php esc_html_e( 'hello there!', 'listings' ); ?>
-    </p>
+    <?php if (empty($listings)) : ?>
+        <div class="no-listings-found">
+            <p><?php esc_html_e('No listings found.', 'listings'); ?></p>
+        </div>
+    <?php else : ?>
+        <div class="listings-container">
+            <?php foreach ($listings as $listing) : ?>
+                <div class="listing-item">
+                    <h3><?php echo esc_html(get_the_title($listing)); ?></h3>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
 </div>
 
 <!-- // $listing_to_show = isset($attributes['listingsToShow']) ? intval($attributes['listingsToShow']) : 10;
